d4d2a0613ec356793bb18f84a2a48f0a9093e8b5
Gloria Dedo <gloriadedo100@gmail.com>
2025-07-09 12:46:02 -0700

    added home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,9 @@
 
-
 export default function Home() {
   return (
     <>
-      <div className="Font-bold text-4xl">
-        <h1>Welcome to Lumea</h1>
-        <p>Your journey to better skin starts here.</p>
+      <div className="flex justify-center items-center">
+     <h1>This is home</h1>
       </div>
     </>
   );
