c89b74a8762ea1eca498a734f24a7cb35ade7ab3
Gloria Dedo <gloriadedo100@gmail.com>
2025-07-14 14:30:55 -0700

    feat: added chats

--- src/Pages/demertologistdashboard/DemCalls.jsx
+++ src/Pages/demertologistdashboard/DemCalls.jsx
@@ -1,12 +1,17 @@
 import Navbar from "@/Components/Navbar";
 import CallsAppointmentCard from "@/Components/Dermetologistcomponent/CallsApointmentcard";
 import MessagingApp from "@/Components/Dermetologistcomponent/Messaging";
+import CometChatNoSSR from "@/CometChat/CometChatNoSSR/CometChatNoSSR";
 export default function DemCalls(){
     return(
         <>
         <Navbar/>
+        <div className="min-h-screen bg-backgrounds ">
+            <CometChatNoSSR/>
+
+        </div>
         
-        <div className="min-h-screen bg-backgrounds flex flex-col p-4 sm:p-8 md:p-12 lg:p-14">
+        {/* <div className="min-h-screen bg-backgrounds flex flex-col p-4 sm:p-8 md:p-12 lg:p-14">
             
             <div className="w-full flex flex-col md:flex-row gap-4">
             <div className="lg:w-[35%] space-y-3  w-full  bg-white p-4 rounded-lg border border-light-border">
@@ -15,12 +20,13 @@ export default function DemCalls(){
             </div  >
             <div className=" md:w-[65%] w-full">
                 <MessagingApp/>
+                
             </div>
             
             </div>
             
             <p>This is Dem calls</p>
-        </div>
+        </div> */}
         </>
     )
 }
